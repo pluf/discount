d3a04686daf681f9b899cef6d2ee47d8d2de8898
hadi <hadi@dpq.co.ir>
2017-09-18 06:14:49 +0430

    improve discount. add REST to check discount validation

--- src/Discount/Views/Discount.php
+++ src/Discount/Views/Discount.php
@@ -6,6 +6,41 @@ class Discount_Views_Discount
 {
 
     /**
+     * Returns discount with given id.
+     *
+     * @param Pluf_HTTP_Request $request
+     * @param array $match 
+     * @return Discount_Discount
+     */
+    public static function get($request, $match)
+    {
+        $discount = Discount_Views_Discount::getDiscount($request, $match);
+        // حق دسترسی
+        // CMS_Precondition::userCanAccessContent($request, $content);
+        // اجرای درخواست
+        return new Pluf_HTTP_Response_Json($discount);
+    }
+
+    /**
+     * Returns discount with given id.
+     *
+     * @param Pluf_HTTP_Request $request
+     * @param array $match$match['code']
+     * @return Discount_Discount
+     */
+    public static function getDiscount($request, $match)
+    {
+        $discount = Pluf_Shortcuts_GetObjectOr404('Discount_Discount', $match['modelId']);
+        $discountEngine = $discount->get_engine();
+        $validationCode = $discountEngine->validate($discount, $request);
+        $discount->validation_code = $validationCode;
+        // حق دسترسی
+        // CMS_Precondition::userCanAccessContent($request, $content);
+        // اجرای درخواست
+        return $discount;
+    }
+
+    /**
      * Returns tag with given name.
      *
      * @param Pluf_HTTP_Request $request
@@ -15,12 +50,15 @@ class Discount_Views_Discount
     public static function getDiscountByCode($request, $match)
     {
         $discount = Discount_Shortcuts_GetDiscountByCodeOr404($match['code']);
+        $discountEngine = $discount->get_engine();
+        $validationCode = $discountEngine->validate($discount, $request);
+        $discount->validation_code = $validationCode;
         // حق دسترسی
         // CMS_Precondition::userCanAccessContent($request, $content);
         // اجرای درخواست
         return $discount;
     }
-    
+
     public static function getByCode($request, $match)
     {
         $discount = Discount_Views_Discount::getDiscountByCode($request, $match);
