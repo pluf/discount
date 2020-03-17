f1c9e6ba9be2747568e1d70e47b8832bd4b57f1e
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2020-03-17 11:12:09 +0330

    Buid config and test configs are updated to pass all unit tests.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Discount System
 
-[![Build Status](https://travis-ci.org/pluf/assort.svg?branch=master)](https://travis-ci.org/pluf/discount)
+[![Build Status](https://travis-ci.com/pluf/discount.svg?branch=master)](https://travis-ci.com/pluf/discount)
+[![codecov](https://codecov.io/gh/pluf/discount/branch/master/graph/badge.svg)](https://codecov.io/gh/pluf/discount)
+[![Maintainability](https://api.codeclimate.com/v1/badges/513f356bdf26065cc009/maintainability)](https://codeclimate.com/github/pluf/discount/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/513f356bdf26065cc009/test_coverage)](https://codeclimate.com/github/pluf/discount/test_coverage)
+[![Coverage Status](https://coveralls.io/repos/github/pluf/discount/badge.svg?branch=master)](https://coveralls.io/github/pluf/discount?branch=master)
 
 This is a Pluf module to provide tools related to discounts. It can be used to manage (add, remove, update, search) different types of discounts.
--- tests/conf/config.php
+++ tests/conf/config.php
@@ -19,18 +19,9 @@ $cfg['url_format'] = 'simple';
 $cfg['tmp_folder'] = '/tmp';
 $cfg['upload_path'] = '/tmp';
 $cfg['middleware_classes'] = array(
-//     'Tenant_Middleware_ResourceAccess',
-    
-    'Pluf_Middleware_Session',
-    'User_Middleware_Session',
-    
-     // find tenant
-     // TODO: replace with tenant midleware
-     'Pluf_Middleware_TenantEmpty',
-     'Pluf_Middleware_TenantFromHeader',
-     'Pluf_Middleware_TenantFromDomain',
-     'Pluf_Middleware_TenantFromSubDomain', // It should be used only in multitenant state
-     'Pluf_Middleware_TenantFromConfig',
+    '\Pluf\Middleware\Tenant',
+    '\Pluf\Middleware\Session',
+    'User_Middleware_Session'
 );
 $cfg['secret_key'] = '5a8d7e0f2aad8bdab8f6eef725412850';
 
@@ -41,7 +32,7 @@ $cfg['templates_folder'] = array(
     dirname(__FILE__) . '/../templates'
 );
 $cfg['template_tags'] = array(
-//     'setting' => 'Tenant_Template_Tag_Setting'
+    // 'setting' => 'Tenant_Template_Tag_Setting'
 );
 $cfg['template_modifiers'] = array();
 
@@ -52,7 +43,6 @@ $cfg['log_level'] = 'error';
 $cfg['log_delayed'] = false;
 $cfg['log_formater'] = '\Pluf\LoggerFormatter\Plain';
 $cfg['log_appender'] = '\Pluf\LoggerAppender\Console';
-
 
 // -------------------------------------------------------------------------
 // Tenants
